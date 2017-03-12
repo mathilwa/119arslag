6e7efbceb01b8ea92b0822c762496618ecd7af4e
mathilwa <mathildewaerstad@gmail.com>
2017-03-12 18:19:06 +0100

    Begynt å få opp inbounce

--- app/frontend/jubilantene/Jubilantene.jsx
+++ app/frontend/jubilantene/Jubilantene.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
 import Aarstall from './Aarstall.jsx';
 
+const byggBarn = (element) => {
+  const barn = [];
+  for (let i = 0; i < element.children.length; i++) {
+    barn.push(element.children[i]);
+  }
+  return barn;
+};
+
 const Jubilantene = React.createClass({
+  componentDidMount () {
+    const tidslinjeBlokk = document.getElementsByClassName('tidslinje-blokk');
+    const tidslinjeElementListe = [];
+    for (let i = 0; i < tidslinjeBlokk.length; i++) {
+      tidslinjeElementListe.push({
+        element: tidslinjeBlokk[i],
+        barn: byggBarn(tidslinjeBlokk[i]),
+      });
+    }
+    tidslinjeElementListe.map(element => {
+      if (element.element.offsetTop > window.top.scrollY + window.innerHeight * 0.75) {
+        element.barn.map(barn => {
+          barn.classList.add('is-hidden');
+        });
+      }
+    });
+    window.addEventListener('scroll', () => this.settSynlighet(tidslinjeElementListe));
+  },
+  componentWillUnmount () {
+    window.removeEventListener('scroll');
+  },
+  settSynlighet (tidslinjeElementListe) {
+    tidslinjeElementListe.forEach(element => {
+      const childrenClassList = element.barn.map(barn => barn.className.toString());
+      if (element.element.offsetTop <= window.top.scrollY + window.innerHeight * 0.75 && childrenClassList[0].includes('is-hidden')) {
+        element.barn.forEach(barn => barn.classList.remove('is-hidden'));
+        element.barn.forEach(barn => barn.classList.add('bounce-in'));
+      }
+    });
+  },
   render () {
     return (
       <div className="information pure-g">
