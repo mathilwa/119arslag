4426174fe1f31ab635e47149941ab14731fe640f
mathildewarstad <mathildewarstad@bekk.no>
2017-04-23 21:31:25 +0200

    Mer informasjon om mamma og pappa

--- app/frontend/jubilantene/informasjon.js
+++ app/frontend/jubilantene/informasjon.js
@@ -13,13 +13,23 @@ export const jubilantene = [
     aar: '1958',
     jubilant: JUBILANT_TROND,
     tittel: 'Lille Trond blir født',
-    tekst: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto, optio, dolorum provident rerum aut hicquasi placeat iure tempora laudantium ipsa ad debitis unde? Iste voluptatibus minus veritatis quiut.',
+    tekst: 'Trond ble født 4. april 1958 på Karolinska Sykehus i Stockholm, Sverige. Han kom ca 10 minutter før sin tvillingsøster Elisabeth (som går under navnet Elle til vanlig). ' +
+    'De ble født i Sverige fordi deres far da gikk på Krigshøgskolan der. Etter kort tid flyttet de til Norge.',
     bildeUrl: 'img/Trond/nyfødt.jpg',
   }, {
     aar: '1960',
     jubilant: JUBILANT_TORUNN,
     tittel: 'Oppvokst på gård',
     tekst: 'Som liten jente vokste Torunn opp på en liten gård på Haugland utenfor Os. Her bodde hun med mamma, pappa, lillebror Tormod og mange forskjellige dyr.',
+    bildeUrl: 'img/Torunn/born0016.JPG',
+  }, {
+    aar: '1960',
+    jubilant: JUBILANT_TORUNN,
+    tittel: 'Søsken',
+    tekst: 'Torunn har en tre år (og to måneder) yngre bror, Tormod. De var venner når de var hjemme alene, men ellers holdt de på med hver sitt. ' +
+    'Som søsken flest hadde de også sine krangler, kanskje spesielt over middagsoppvasken på søndagane hvor Torunn ALLTID måtte vaske, mens han ' +
+    'slapp unna med å tørke. I perioder sov de på samme rom, hvor hobbyen var å telle lysstripet i taket når bilene kjørte forbi ' +
+    '(noe som på den tida tok tid å få noe tall å skryte av).',
     bildeUrl: 'img/Torunn/born0016.JPG',
   }, {
     aar: '1961',
@@ -55,7 +65,7 @@ export const jubilantene = [
     'men Torunn og Tormod prøvde så langt det lot seg gjøre å unngå arbeidsoppgaver. De var aldri med i fjøset, og Torunn husker med gru all ripsen som skulle plukkest til saft. Torunn prøvde ' +
     'også å unngå klesvasken, da det var veldig tungt og kjedsommelig arbeid. Klesvasken foregikk i kjelleren, med koking av klær i diger kjele på den gamle vedovnen, og skovlemaskin med vriapparat. ' +
     'Alt ble skylt i kaldt vann i store stamper og hengt ut på snora. Når klærne var ute var det viktig å følge med på været, og ta inn klærne før regnet kom. Torunn hadde som jobb å vaska ' +
-    'soverom og gang hver fredag, noe hun fikk 10 kr. Det som ble sett frem til av arbeidoppgaver var våronna, fordi det da var fullt av folk på gården.',
+    'soverom og gang hver fredag, noe hun fikk 10 kr. Det som ble sett frem til av arbeidoppgaver var onnene, fordi det da var fullt av folk på gården.',
     bildeUrl: 'img/Torunn/Oppvokst på gård.jpg',
   }, {
     aar: '1964',
@@ -64,19 +74,53 @@ export const jubilantene = [
     tekst: 'Trond sammen med pappa Arnulf Odd og lillebror Nils Henrik på Nils sin dåpsdag.',
     bildeUrl: 'img/Trond/Nils dåp 3.jpg',
   }, {
-    aar: '1969',
+    aar: '1965',
+    jubilant: JUBILANT_TROND,
+    tittel: 'Isreal',
+    tekst: 'I 1965 tok Arnulf Odd med seg hele familien i en stasjonsvogn og dro til Jerusalem som FN-observatør. De bodde 6 måneder i Jerusalem og 6 måneder i Tiberias. ' +
+    'En spennende tid hvor de fikk oppleve mange historiske steder. Trond fikk sin første ridetur, på kamel, 17. mai i 1966, noe som var ganske skummelt for en liten gutt. ' +
+    'Dette var ikke bare ferie for Trond, da han måtte gjøre hjemmelekser siden han egentlig skulle gått på skolen i Norge. Da han kom tilbake til Norge begynte han rett i 2. klasse på ' +
+    'folkeskolen. Trond hadde fått seg en «puslering» i sølv da han var i Israel. Denne var låst med en liten bøyd ståltråd slik at den ikke skulle falle fra hverandre. ' +
+    'Etter at han var kommet hjem fjernet Trond låsen og fikk store problemer med å sette den sammen igjen. Trond ble livredd for at moren eller faren skulle komme hjem før ' +
+    'han fikk satt den sammen igjen. Selvfølgelig uten grunn, men han klarte å sette den sammen igjen etter utallige forsøk.',
+    bildeUrl: 'img/Trond/israel.jpg',
+  }, {
+    aar: '1967',
     jubilant: JUBILANT_TORUNN,
     tittel: 'Håret',
     tekst: 'Torunn hadde ekstremt langt hår som ung. Det var til tider så langt at hun kunne sitte på det. Uten balsam og ikke noe annet enn kam kunne det være en ganske vond affære' +
     ' for ei hårsår jente å få dette ordnet.',
     bildeUrl: 'img/Torunn/langt hår.jpg',
   }, {
+    aar: '1968',
+    jubilant: JUBILANT_TROND,
+    tittel: 'Lederhosen',
+    tekst: 'Etter å ha vært på militærkurs i Tyskland hadde Arnulf Odd med seg gave hjem til Trond, en «lederhosen», som var en lærshorts med skulderremmer og frontstykke. ' +
+    'Han ble pålagt å gå med denne på skolen, noe som bare gjorde Trond til et fantastisk mobbeobjekt! Det ble (heldigvis) bare med den ene dagen.',
+    bildeUrl: 'img/Trond/lederhosen.jpg',
+  }, {
+    aar: '1969',
+    jubilant: JUBILANT_TROND,
+    tittel: 'Ustaoset',
+    tekst: 'Familien til Trond hadde hytte på Ustaoset, hvor de tilbrakt mange vintrer og påskeferier.',
+    bildeUrl: 'img/Trond/på snøtaket.jpg',
+  }, {
     aar: '1970',
     jubilant: JUBILANT_TORUNN,
     tittel: 'Leikarring',
-    tekst: 'Torunn likte godt å danse, og gikk på gammeldanskurs og leikraring. Torunn var ikke på mange bygdedanser, men dansa når det var mulighet. ' +
+    tekst: 'Torunn likte godt å danse, og gikk på gammeldanskurs og leikarring. Torunn var ikke på mange bygdedanser, men dansa når det var mulighet. ' +
     'Bildet her er fra avslutninga i 6. klasse. Dans var også hovedhobbyen da Torunn bodde i Trondheim. Hun var da med i BUL og danset gammeldans, selv om det var swing som var det store.',
     bildeUrl: 'img/Torunn/Leikarring.jpg',
+  }, {
+    aar: '1972',
+    jubilant: JUBILANT_TROND,
+    tittel: 'Telegrafalléen',
+    tekst: 'Trond og familien flyttet etterhvert til Telegrafalléen 4 i Hønefoss. Det var et kjempestort hus med flere titalls dører, og Trond gikk seg ofte vill i begynnelsen. ' +
+    'Huset var fyrt med ved og kull, så om vinteren måtte Trond opp og hugge ved og fyre i ovnene slik at det ikke skulle være så kaldt. Det var likevel ikke mulig å få varmet opp hele huset, ' +
+    'så det var bare vinduene på rommet man kunne se ut av på den årstiden. I resten av huset var det fullt av is - på innsiden! Det var ofte gjerne bare rett over 0 grader ' +
+    'på rommet til Trond da han stod opp. Dette ble bedre da familien arvet Koleman’en fra bestemoren. Det var en parafinbrenner som klunket og gikk og holdt rommet nogenlunde varmt, og Trond ' +
+    'likte godt å stå å varme ryggen mot klebersteinsovnen i stua.',
+    bildeUrl: 'img/Trond/familien.jpg',
   }, {
     aar: '1972',
     jubilant: JUBILANT_TORUNN,
@@ -87,9 +131,10 @@ export const jubilantene = [
   }, {
     aar: '1973',
     jubilant: JUBILANT_TROND,
-    tittel: 'Trond blir konfirmert',
-    tekst: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-    bildeUrl: 'img/Trond/Dress og konfirmasjon.jpg',
+    tittel: 'På skolen',
+    tekst: 'Trond og Elle gikk i samme klasse på Eikeli Folkeskole i Hønefoss. Der sang han i kor og spilte baryton, men ingen av delen varte lenge da interessen for fotball var større. ' +
+    'Trond var veldig glad i matematikk og det ble en sport mellom ham og en medelev å gjøre mest mulig av oppgavene til dagen etter. De var ferdige med 7. klassepensum i 6. klasse.',
+    bildeUrl: 'img/Trond/Konfirmasjon.jpg',
   }, {
     aar: '1973',
     jubilant: JUBILANT_TORUNN,
@@ -126,9 +171,21 @@ export const jubilantene = [
     aar: '1977',
     jubilant: JUBILANT_TROND,
     tittel: 'Hobbymekaniker',
-    tekst: 'Trond har fra han var veldig ung hatt en stor interesse for å mekke bil. Dette er en fascinasjon og glede som ikke akkurat har avtatt med åren, ' +
-    'og listen over biler og tilhørende bildeler kjøpt og jobbet på er tilnærmet lik uendelig lang.',
+    tekst: 'Trond har fra han var veldig ung hatt en stor interesse for elektriske ting. Han leste alt han kom over av elektroniske blader, og brukte ofte fritiden sin som hobbymekaniker. ' +
+    'Trond var en enstøing som i stedet for å dra på fest gjerne satt hjemme og demonterte radioer, koblet opp telefonlinjer, eller skrudde på bil eller mopedene til kameratene. ' +
+    'Dette er en fascinasjon og glede som ikke akkurat har avtatt med åren, og listen over biler og tilhørende bildeler kjøpt og jobbet på er tilnærmet lik uendelig lang.',
     bildeUrl: 'img/Trond/pappa mekker bil.jpg',
+  }, {
+    aar: '1978',
+    jubilant: JUBILANT_TROND,
+    tittel: 'Uhell',
+    tekst: 'I forbindelse med at Trond en gang som ung skulle fikse bremsene på familiens bil trengte han å varme opp noe. Faren hadde en militær blåselampe som gikk på parafin. ' +
+    'Den funker slik at man pumper til man får trykk, og når den var varm nok blåser den en fin, tykk flamme. Denne prøvde Trond utenfor hovedinngange. Da Trond trodde den var ' +
+    'varm nok åpnet han for hovedflammen. Den kom ikke, men i stedet sendte lampen ut en laaang stråle med brennende parafin som traff like ved garasjen. Uheledigvis akkurat samme sted som ' +
+    'fjorårets svært tørre juletre sto plassert. Det tok fyr, som videre satte garasjen i brann. Trond ropte inn at noen skulle ringe brannvesnet, mens han selv løp ned i kjelleren ' +
+    'for å sette på vannslangen. Innen han  kom tilbake var alt allerede på plass: brannbil, politi og tilskuere. De hadde sett røyken fra brannstasjonen og kom umiddelbart. Garasjen ' +
+    'fikk heldigvis bare innvendige skader.',
+    bildeUrl: 'img/Trond/møkkete.jpg',
   }, {
     aar: '1978',
     jubilant: JUBILANT_TORUNN,
@@ -153,13 +210,13 @@ export const jubilantene = [
     tekst: 'Etter å ha vært i militæret sammen, gått på samme skole ett helt år, og tilbrakt masse tid sammen på befalsmessa uten å legge merke til hverandre, ' +
     'ble de endelig kjent. Noen måneder til skulle det gå fra dette tidspunktet til de ble kjærester. De fikk dessverre ikke lange tiden sammen før Trond ' +
     'ble beordret til Nord-Norge.',
-    bildeUrl: 'img/begge/de møtes.JPG',
+    bildeUrl: 'img/begge/de møtes.jpg',
   }, {
     aar: '1981',
     jubilant: JUBILANT_TORUNN,
     tittel: 'Blir igjen i Trondheim',
-    tekst: 'Torunn blir igjen i Trondheim når Trond reiser nordover, og begynner å jobbe på Elab på NTH (dagens NTNU), hvor hun ble værende i 5 år.  ' +
-    'Torunn ble i løpet av Ingeniørhøgskolen utdannet elektroingeniør, men flytta seg i denne jobben over mot det digitale, som var mye mer spennende. ' +
+    tekst: 'Torunn ble igjen i Trondheim da Trond reiser nordover, og begynte å jobbe på Elab på NTH (dagens NTNU), hvor hun ble værende i 5 år.  ' +
+    'Torunn ble i løpet av Ingeniørhøgskolen utdannet elektroingeniør, men flyttet seg i denne jobben over mot det digitale, som var mye mer spennende. ' +
     'Hun ble hekta på programmering, og et morsomt, men veldig spennende prosjekt var å få være med å bygge Norges første GPS-mottakar.',
     bildeUrl: 'img/Torunn/ELab.jpg',
   }, {
@@ -186,13 +243,13 @@ export const jubilantene = [
     aar: '1986',
     jubilant: JUBILANT_TORUNN,
     tittel: 'Ny jobb',
-    tekst: 'Torunn begynner å jobbe for Simrad, nå Kongsberg Maritime, i Horten. En fantastisk spennende jobb hvor hun har fått vært med å utvikle verdens beste ekkolodd.',
+    tekst: 'I Horten begynte Torunn å jobbe for Simrad, nå Kongsberg Maritime. En fantastisk spennende jobb hvor hun har fått vært med å utvikle verdens beste ekkolodd.',
     bildeUrl: 'img/begge/1988_0002.jpg',
   }, {
     aar: '1987',
     jubilant: JUBILANT_BEGGE,
     tittel: 'Torunn og Trond blir foreldre',
-    tekst: 'Høsten 1987, kun ett år etter at Torunn og Trond flyttet til Horten, blir de foreldre til lille Mathilde.',
+    tekst: 'Høsten 1987, kun ett år etter at Torunn og Trond flyttet til Horten, ble de foreldre til lille Mathilde.',
     bildeUrl: 'img/begge/1988_0002.jpg',
   }, {
     aar: '1990',
@@ -200,6 +257,24 @@ export const jubilantene = [
     tittel: 'Thomas blir født',
     tekst: 'I februar 1990 blir Torunn og Trond foreldre igjen, denne gangen til en nydelige, liten gutt som skulle få navnet Thomas.',
     bildeUrl: 'img/begge/Lille Thomas.jpg',
+  }, {
+    aar: '1990',
+    jubilant: JUBILANT_BEGGE,
+    tittel: 'Fint sted å bo',
+    tekst: 'Da Horten ble valgt som bosted, hadde ikke Torunn og Trond tenkt så mye på om det var en hyggelig by, om det var fine områder eller om det fantes kulturelle tilbud. ' +
+    'Dette valget ble rett og slett tatt på bakgrunn av det gode elektronikkmiljøet - noe som var viktig for dem begge. Heldigvis viste Horten og Falkensten ' +
+    'seg som et trygt og fint sted å bo, kanskje spesielt med små barn. Falkensten var et lite område hvor alle kjente alle, med et hyggelig lokalmiljø. ' +
+    'Her ble det arrangert feiringer når det passet seg, for eksempel til jul og på 17. mai.',
+    bildeUrl: 'img/begge/17mai pappa og barna.jpg',
+  }, {
+    aar: '1990',
+    jubilant: JUBILANT_TROND,
+    tittel: 'Hytta',
+    tekst: 'I nærheten av Ulefoss i Telemark finner man røttene til Wærstadfamilien, og like ved det som kalles Wærstad-gården hadde Trond og familie en hytte. Ikke store greiene, ' +
+    'med 2 soverom, 2 mindre stuer, et kjøkken og loft, spesielt ikke når man plutselig skulle samle hele familien (mor, far, søsken og alle ders barn). Trangt var det, men desto hyggeligere' +
+    ' (og høylytt) kunne det bli. Hytta hadde ikke innlagt vann, og den hadde også utedo i skogkanten. Heldigvis var det nok av uteområder, og et høydepunkt for hver samling var løypa i skogen hvor man' +
+    ' skulle prøve å finne gjemte ting som ikke hørte til der. En annen hyggelig tradisjon var å plukke blåbær, da skogenbunnen til tider var blå så langt øyet kunne se.',
+    bildeUrl: 'img/Trond/Hytta.jpg',
   }, {
     aar: '1995',
     jubilant: JUBILANT_TORUNN,
@@ -226,10 +301,10 @@ export const jubilantene = [
     aar: '1995',
     jubilant: JUBILANT_TROND,
     tittel: 'Ribbemannen',
-    tekst: 'I timene før julen ringes inn er Trond å finne inne på kjøkkenet, iført forkle, hvor han står foran ovnen og strengt vokter ribba. Ribba skal være saftig og god, ' +
-    'men viktigst av alt er svoren. Dette er et suksessfullt prosjekt hver eneste år - knasende sprø ender den alltid opp med å være! Torunn, ' +
+    tekst: 'I timene før julen ringes inn er Trond å finne inne på kjøkkenet, iført forkle, hvor han står foran ovnen og vokter ribba. Ribba skal være saftig og god, ' +
+    'men viktigst av alt er svoren. Dette er et suksessfullt prosjekt hver eneste år - den ender alltid opp med å være knasende sprø! Torunn, ' +
     'med sine vestlandsrøtter, har flere ganger prøvd å få gjennom både pinnekjøtt og lutefisk, men her har Trond (heldigvis) stått på kravene. ' +
-    'Det blir ikke jul uten ribbe. Da vi i 1998 feiret jul på Haugland hos Torunn sin familie, ble det derfor servert både pinnekjøtt og ribbe. ',
+    'Det blir ikke jul uten ribbe. Da vi i 1995 feiret jul på Haugland hos Torunn sin familie, ble det derfor servert både pinnekjøtt og ribbe. ',
     bildeUrl: 'img/Trond/ribbemannen.jpg',
   }, {
     aar: '2000',
@@ -279,6 +354,16 @@ export const jubilantene = [
     bildeUrl: 'img/Torunn/Nidarholm1.jpg',
   }, {
     aar: '2013',
+    jubilant: JUBILANT_TORUNN,
+    tittel: 'Sykkeltur langs Donau',
+    tekst: 'At Torunn og Mathilde drar på sykkeltur sammen har vært en årlig agenda for sommerferien siden sykkelturen til Bergen. Turene har da vært rundt om i vakre Norge. Sommeren 2013 ' +
+    'ville Torunn og Mathilde prøve noe nytt, og de ville ha med seg lille niesa/kusina Ester på lasset. Denne gangen skulle turen gå til utlandet, nærmere bestemt Østerriket. Etter et par flyturer ' +
+    'og en ganske så oppstykket togtur (som inkluderte noen avslappende timer på noe avispapir på en forlatt togstasjon midt på natta), så kom vi frem til Passau hvor turen skulle starte. ' +
+    'Turen skulle vare i 7 dager, og i løpet av denne tiden skulle vi sykle langs Donau i fantastisk natur, se spennende bygg og småbygder, besøke vingårer, spise masse god mat, få vonde rumper ' +
+    'og ',
+    bildeUrl: 'img/Torunn/sykkeltur donau.jpg',
+  }, {
+    aar: '2014',
     jubilant: JUBILANT_TORUNN,
     tittel: 'Gjøre det selv',
     tekst: 'Som datter av en mor som har drevet en gård og oppdratt to barn alene, er Torunn vant til å ta tak og gjøre ting selv. ' +
